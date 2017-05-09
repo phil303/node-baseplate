21d1ac27cc2de90a401285281ea195fc5d5ccc8d
Phil Aquilina <philaquilina@gmail.com>
2017-05-09 09:26:22 -0700

    add todo list

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ module.exports = {
   configureMetrics: metrics.configureMetrics,
   configureLogging: logging.configureLogging,
 };
+
+
+// TODO:
+// BASIC:
+//   - thrift istallation and compiler
+//   - retry logic
+//   - http client
+//   - configuration validation
+//   - how does logging to file work in prod
+//   - checking prod metrics actually work
+//
+// MEDIUM TERM:
+//   - tests
+//   - event queue process
+//   - source mapping
+//   - cookiecutter
+//   - README/tutorial
